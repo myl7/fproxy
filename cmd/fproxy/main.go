75eef01e91c308b51a7895aa074363b5dab3b952
myl7 <myl@myl.moe>
2022-10-09 05:55:08 +0800

    Add flag cli

--- cmd/fproxy/main.go
+++ cmd/fproxy/main.go
@@ -4,15 +4,27 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	"github.com/myl7/fproxy"
 )
 
 func main() {
+	scheme := flag.String("scheme", "https", "scheme")
+	host := flag.String("host", "", "host")
+	pPrefix := flag.String("pPrefix", "", "path prefix")
+	cacheDir := flag.String("cacheDir", "", "cache dir")
+	listen := flag.String("listen", ":8000", "listen")
+	flag.Parse()
+
+	if *host == "" {
+		panic("host is invalid")
+	}
+
 	p := fproxy.NewProxy(fproxy.Config{
-		URLTransform:   fproxy.URLTransformPrefix("https", "share.myl.moe", "", false),
-		URLToLocalPath: fproxy.URLToLocalPathPrefix("cache"),
+		URLTransform:   fproxy.URLTransformPrefix(*scheme, *host, *pPrefix, false),
+		URLToLocalPath: fproxy.URLToLocalPathPrefix(*cacheDir),
 	})
-	http.ListenAndServe(":8000", p)
+	http.ListenAndServe(*listen, p)
 }
